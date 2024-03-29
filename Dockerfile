f4f5ac8a80359189065b6e76a3b97a7c7c1f0161
lizasaigina <lz.sygn@gmail.com>
2024-03-28 21:28:22 -0400

    test2

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,7 @@ COPY . ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 8080
+EXPOSE 80
 
 # Run app.py when the container launches
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
--- app/database/session.py
+++ app/database/session.py
@@ -2,29 +2,36 @@ import os
 import json
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from google.cloud.sql.connector import Connector, IPTypes
 
 
 # Get database credentials from the DBCRED environment variable
 db_credentials = json.loads(os.environ["DBCRED"])
 
-# Construct the database connection string
-db_uri = (
-    f"postgresql+pg8000://{db_credentials['user']}:{db_credentials['password']}"
-    f"@/{db_credentials['database']}?host=/cloudsql/{db_credentials['project']}:"
-    f"{db_credentials['region']}:{db_credentials['instance']}" 
-)
+# Configure connector
+connector = Connector()  
 
-# Create a SQLAlchemy engine
-db_engine = create_engine(db_uri)
+def getconn() -> sqlalchemy.engine.base.Connection:
+    conn = connector.connect(
+        db_credentials['project'] + ':' + db_credentials['region'] + ':' + db_credentials['instance'],
+        "pg8000",
+        user=db_credentials['user'],
+        password=db_credentials['password'],
+        db=db_credentials['database'],
+        ip_type=IPTypes.PUBLIC  # Use PRIVATE if connecting via private IP
+    )
+    return conn
+
+# Create a SQLAlchemy engine with Cloud SQL Connector
+db_engine = create_engine(
+    "postgresql+pg8000://",
+    creator=getconn,
+)
 
 # Create a sessionmaker
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
 def get_db():
-    print(db_credentials)
-    print()
-    print(db_uri)
-    print()
     db = SessionLocal()
     try:
         yield db
