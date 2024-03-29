9fdc24287fd536aad4b62029943ad6628af2e481
lizasaigina <lz.sygn@gmail.com>
2024-03-28 20:08:29 -0400

    env credentials used for db access

--- app/database/session.py
+++ app/database/session.py
@@ -1,6 +1,6 @@
+import os
 import json
 from google.cloud.sql.connector import Connector, IPTypes
-from google.cloud import secretmanager_v1
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 
@@ -9,40 +9,18 @@ from sqlalchemy.orm import sessionmaker
 connector = Connector()
 
 
-# Function to get database credentials from Google Secret Manager
-def get_db_credentials(
-    secret_name: str, project_id: str, version_id: str = "latest"
-) -> dict:
-    # Create a Secret Manager client
-    client = secretmanager_v1.SecretManagerServiceClient()
-
-    # Build the resource name of the secret version
-    name = f"projects/{project_id}/secrets/{secret_name}/versions/{version_id}"
-
-    # Access the secret version
-    response = client.access_secret_version(request={"name": name})
-
-    # Decode the secret payload
-    secret_string = response.payload.data.decode("UTF-8")
-
-    # Parse the secret payload into a dictionary
-    secret_dict = json.loads(secret_string)
-
-    return secret_dict
-
-
-# Retrieve the database credentials
-credentials = get_db_credentials("db-credentials", "477367316624")
+# Get database credentials from the DBCRED environment variable
+db_credentials = json.loads(os.environ["DBCRED"])
 
 
 # Define a function to create a database connection
 def getconn() -> sqlalchemy.engine.base.Connection:
     conn = connector.connect(
-        credentials["project"],
+        db_credentials["project"],
         "pg8000",
-        user=credentials["user"],
-        password=credentials["password"],
-        db=credentials["database"],
+        user=db_credentials["user"],
+        password=db_credentials["password"],
+        db=db_credentials["database"],
         ip_type=IPTypes.PUBLIC,  # Use PRIVATE for private IP or PUBLIC for public IP
     )
     return conn
