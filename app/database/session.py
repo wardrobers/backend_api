90d14f2cb0aadf0069e5770e32ac1af02be3ad12
lizasaigina <lz.sygn@gmail.com>
2024-03-28 20:15:55 -0400

    connector to Cloud SQL updated

--- app/database/session.py
+++ app/database/session.py
@@ -1,40 +1,24 @@
 import os
 import json
-from google.cloud.sql.connector import Connector, IPTypes
-import sqlalchemy
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-
-# Initialize the Connector object
-connector = Connector()
 
 
 # Get database credentials from the DBCRED environment variable
 db_credentials = json.loads(os.environ["DBCRED"])
 
-
-# Define a function to create a database connection
-def getconn() -> sqlalchemy.engine.base.Connection:
-    conn = connector.connect(
-        db_credentials["project"],
-        "pg8000",
-        user=db_credentials["user"],
-        password=db_credentials["password"],
-        db=db_credentials["database"],
-        ip_type=IPTypes.PUBLIC,  # Use PRIVATE for private IP or PUBLIC for public IP
-    )
-    return conn
-
-
-# Create a SQLAlchemy engine using the connection created by the Connector
-db_engine = sqlalchemy.create_engine(
-    "postgresql+pg8000://",
-    creator=getconn,
+# Construct the database connection string
+db_uri = (
+    f"postgresql+pg8000://{db_credentials['user']}:{db_credentials['password']}"
+    f"@/{db_credentials['database']}?host=/cloudsql/{db_credentials['project']}:"
+    f"{db_credentials['region']}:{db_credentials['instance']}" 
 )
 
-# Create a new sessionmaker using the engine
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+# Create a SQLAlchemy engine
+db_engine = create_engine(db_uri)
 
+# Create a sessionmaker
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
 def get_db():
     db = SessionLocal()
