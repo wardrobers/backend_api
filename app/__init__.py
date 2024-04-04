26f1506841178f9771c03b713a683b4cc7ce6c5c
NekrasovN <nekrasov.job@gmail.com>
2024-04-04 07:52:12 +0300

    3
    3

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .routers.user import user_router
+from .routers.user import user_router,user_test_router
 from .routers.product import product_router
 from .routers.order import order_router
 
@@ -9,6 +9,7 @@ api_router = APIRouter()
 # Bundle router inclusions for clarity and centralized error handling
 def include_api_routers():
     api_router.include_router(user_router.router, prefix="/users", tags=["Users"])
+    api_router.include_router(user_test_router.router, prefix="/test/users", tags=["Users_test"])
     api_router.include_router(
         product_router.router, prefix="/products", tags=["Products"]
     )
--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,6 +1,7 @@
-from .user import auth_router, user_router, user_test_router
+from .user import auth_router, user_router
 from .order import order_router
 from .product import product_router
+from .user import user_test_router
 
 
 # from app.routers import auth_router, user_router, order_router, product_router
